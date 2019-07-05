20db0e2f03bc3f03557ad6481dd2c3c5c0d0fa83
Mohiul Haque <44129090+mohaque0@users.noreply.github.com>
2019-07-04 23:56:02 -0400

    Bumping version to v0.2-alpha.

--- includes/cradle_main.hpp
+++ includes/cradle_main.hpp
@@ -22,7 +22,7 @@
 	}                                         \
 	void configure();                         \
 	int main(int argc, char** argv) {         \
-	  log("Cradle Version v0.1-alpha");       \
+	  log("Cradle Version v0.2-alpha");       \
 	  cradle::platform::platform_chdir(cradle::io::path_parent(getBuildConfigFile())); \
 	  parseCmdLineArgs(argc, argv);           \
 	  configure();                            \
